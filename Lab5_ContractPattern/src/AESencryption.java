348289e7ea89a486110f452e96e7346fbc695141
MinhNghiaZ <167590885+MinhNghiaZ@users.noreply.github.com>
2025-05-31 08:17:21 +0700

    V1FMP
    upload Folder

--- Lab5_ContractPattern/src/AESencryption.java
+++ Lab5_ContractPattern/src/AESencryption.java
@@ -1,6 +1,4 @@
 import javax.crypto.Cipher;
-import javax.crypto.SecretKey;
-import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
 import java.security.MessageDigest;
